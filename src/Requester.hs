f71befdcb85bc5695270b6f70067ed4e3a777f31
Vitor Roque <vitor.roquep@gmail.com>
2024-02-01 19:50:42 -0300

    fix: fixed tests

--- src/Requester.hs
+++ src/Requester.hs
@@ -1,6 +1,3 @@
 module Requester () where
 
 import Data.ByteString.Lazy (ByteString)
-
-handlePacket :: ByteString -> Request
-handlePacket packet =
--- test/Main.hs
+++ test/Main.hs
@@ -1,14 +1,20 @@
 module Main (main) where
 
 import Test.HUnit
+  ( runTestTT,
+    Counts(errors, failures),
+    Test(TestList)
+  )
 import ConfigTest (configTests)
-import RequesterTest (requesterTests)
-import qualified System.Exit as Exit
+--import RequesterTest (requesterTests)
 import Control.Monad (when)
+import qualified System.Exit as Exit
+
+tests :: Test
+tests = TestList [configTests]
 
 main :: IO ()
 main = do
-  result <- runTestTT $ requesterTests
+  result <- runTestTT tests
   when (failures result > 0) $ Exit.exitFailure
   when (errors result > 0) $ Exit.exitFailure
-  Exit.exitSuccess
